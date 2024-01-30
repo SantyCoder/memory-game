3562fdd4aba9f44df4027cb7f7d6f57b0cbf7ada
Santiago Marcelino Flores <santiago.marcelino@gigigo.com>
2024-01-29 19:10:16 -0600

    MEMOGAME-05 Styles are updated for mobile version

--- source/src/app/app.module.ts
+++ source/src/app/app.module.ts
@@ -17,6 +17,23 @@ import { MemoryMarvelComponent } from './public/memory-marvel/memory-marvel.comp
 import { TabsComponent } from './public/tabs/tabs.component';
 import { ResumeComponent } from './public/resume/resume.component';
 import { MenuComponent } from './template/menu/menu.component';
+//Firebase
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+//
+const firebaseConfig = {
+  apiKey: "AIzaSyB-rDMjn67sEJ03WK6TwsHZhR3cNf_hOAE",
+  authDomain: "santiago-marcelino-flores.firebaseapp.com",
+  projectId: "santiago-marcelino-flores",
+  storageBucket: "santiago-marcelino-flores.appspot.com",
+  messagingSenderId: "42484585207",
+  appId: "1:42484585207:web:6c8ef7b1a8acf3e90c39f2",
+  measurementId: "G-NCKXD5BCNS"
+};
+//
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+//
 @NgModule({
   declarations: [
     AppComponent,
